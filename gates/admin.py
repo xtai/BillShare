c254043c16dbda7f333abefac2439dd5c5aefd39
Xiaoyu Tai <taixiaoyu@gmail.com>
2015-07-08 19:29:34 +0800

    merge admin.py

--- gates/admin.py
+++ gates/admin.py
@@ -7,12 +7,8 @@ class RecordInline(admin.TabularInline):
     model = Record
     extra = 1
 
-<<<<<<< HEAD
 
-class ProjectAdmin(admin.ModelAdmin):
-=======
 class GroupAdmin(admin.ModelAdmin):
->>>>>>> b30bd39e38faf08a11245cea28f5001676457fc2
     fieldsets = [
         (None, {'fields': ['name', 'desc']}),
         ('Members', {'fields': ['members']}),
@@ -24,7 +20,6 @@ class GroupAdmin(admin.ModelAdmin):
     search_fields = ['name', 'desc']
 
 
-<<<<<<< HEAD
 class RecordAdmin(admin.ModelAdmin):
     list_display = ('name', 'group', 'id', 'amount', 'note',
                     'payer', 'receiver', 'creation_date', 'last_change_date')
@@ -32,9 +27,5 @@ class RecordAdmin(admin.ModelAdmin):
     search_fields = ['name', 'note']
 
 
-admin.site.register(Project, ProjectAdmin)
-admin.site.register(Record, RecordAdmin)
-=======
 admin.site.register(Group, GroupAdmin)
-admin.site.register(Record)
->>>>>>> b30bd39e38faf08a11245cea28f5001676457fc2
+admin.site.register(Record, RecordAdmin)
