d3d090973a0eb6b1700e8b871d709e173e26a69f
Xiaoyu Tai <taixiaoyu@gmail.com>
2015-07-08 19:23:08 +0800

    add admin specs

--- gates/admin.py
+++ gates/admin.py
@@ -7,6 +7,7 @@ class RecordInline(admin.TabularInline):
     model = Record
     extra = 1
 
+
 class ProjectAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['name', 'desc']}),
@@ -14,10 +15,17 @@ class ProjectAdmin(admin.ModelAdmin):
         ('Creation Date', {'fields': ['creation_date']}),
     ]
     inlines = [RecordInline]
-    list_display = ('name', 'desc', 'creation_date', 'last_change_date')
+    list_display = ('name', 'id', 'desc', 'creation_date', 'last_change_date')
     list_filter = ['creation_date']
     search_fields = ['name', 'desc']
 
 
+class RecordAdmin(admin.ModelAdmin):
+    list_display = ('name', 'group', 'id', 'amount', 'note',
+                    'payer', 'receiver', 'creation_date', 'last_change_date')
+    list_filter = ['creation_date', 'payer', 'receiver']
+    search_fields = ['name', 'note']
+
+
 admin.site.register(Project, ProjectAdmin)
-admin.site.register(Record)
+admin.site.register(Record, RecordAdmin)
